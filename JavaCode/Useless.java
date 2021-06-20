18eead10cd3afe1adce33fe23c320ed6e7db4207
Chand <2017eeb1136@iitrpr.ac.in>
2021-06-20 12:01:01 +0530

    Added functions in Useless.java added description in readme

--- JavaCode/Useless.java
+++ JavaCode/Useless.java
@@ -1,4 +1,11 @@
-public class Useless(){
+public class Useless{
 	// something
 	// this is updated online.
+	public static void main(String args[]){
+		System.out.println("Hello, world, this is useless main function");
+		helperFunction();
+	}
+	public static void  helperFunction(){
+		System.out.println("Insise helper Function");
+	}
 }
